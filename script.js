a1958331a3d2daa3ce3296c1f060c9922c151a37
Theo Phofedi <themotouch02@gmail.com>
2026-05-14 14:41:12 +0200

    script.js

---
+++ script.js
@@ -0,0 +1,37 @@
+let cart = [];
+let total = 0;
+
+function addToCart(name, price) {
+  cart.push({name, price});
+  total += price;
+  alert(name + " added to cart");
+}
+
+function openCart() {
+  document.getElementById("cart").style.display = "block";
+
+  let list = document.getElementById("cartItems");
+  list.innerHTML = "";
+
+  cart.forEach(item => {
+    let li = document.createElement("li");
+    li.textContent = item.name + " - P" + item.price;
+    list.appendChild(li);
+  });
+
+  document.getElementById("total").textContent = total;
+}
+
+function goToCheckout() {
+  document.getElementById("checkout").style.display = "block";
+}
+
+function completePurchase() {
+  alert("Payment Successful! Thank you for shopping.");
+
+  cart = [];
+  total = 0;
+
+  document.getElementById("cartItems").innerHTML = "";
+  document.getElementById("total").textContent = "0";
+}
